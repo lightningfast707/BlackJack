4d1a06806595cf27c24d55572bb36787648d52cb
lightningfast707 <khoi304857@gmail.com>
2025-04-11 11:58:26 -0700

    blackjack handout-3: create player class

---
+++ Player.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+
+public class Player 
+{
+   private ArrayList<Card> hand;
+   private int winCount;
+
+   public Player ()
+   {
+      hand = new ArrayList<Card>();
+      this.winCount = 0;
+   }
+
+   public Player (int score)
+   {
+      hand = new ArrayList<Card>();
+      this.winCount = score;
+   }
+
+   public void addCardToHand( Card temp )
+   {
+      hand.add(temp);
+   }
+
+   public void resetHand( )
+   {
+      hand.clear();
+   }
+
+   public  void setWinCount( int numwins )
+   {
+      this.winCount = numwins;
+   }
+
+   public int getWinCount() 
+   {
+     return winCount; 
+   }
+
+   public int getHandSize() 
+   { 
+     return hand.size(); 
+   }
+
+   public int getHandValue()
+   {
+      int value = 0;
+      for (int i = 0; i < hand.size(); i++)
+      {
+        value+= hand.get(i).getValue();
+      }
+      return value;
+   }
+
+   public boolean hit( )
+   {
+      return false;
+   }
+
+   public String toString()
+   {
+      return "Hand:" + "" + hand + " Hand Value:" + "" + getHandValue() + " Win Count:" + "" +  winCount;
+   }
+
+}
---
+++ PlayerTestOne.java
@@ -0,0 +1,29 @@
+public class PlayerTestOne 
+{
+    public static void main( String args[] )
+	{
+		Player player = new Player();	
+		
+		Deck deck = new Deck();
+		deck.shuffle();
+		
+		player.addCardToHand(deck.nextCard());
+		player.addCardToHand(deck.nextCard());
+
+		System.out.println("\n\ntoString");
+		System.out.println(player);	
+		
+		System.out.println("\n\nhandValue");
+		System.out.println(player.getHandValue());					
+		
+		player.addCardToHand(deck.nextCard());
+		player.addCardToHand(deck.nextCard());
+
+		System.out.println("\n\ntoString");
+		System.out.println(player);	
+		
+		System.out.println("\n\nhandValue");
+		System.out.println(player.getHandValue());									
+	}
+    
+}
